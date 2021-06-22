84cab578a9db1b5cda5550d5b823ee6d310fc097
Elton <eltonsa75@hotmail.com>
2021-06-22 12:37:59 -0300

    Criando  a index do gráfico

---
+++ Dashboard2/conexao/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost"; //padrão - server local
+$username = "root"; //padrão - root
+$password = "hodel"; //senha de conexão do banco de dados.
+$database = "vendas"; //alterar conforme o nome do seu banco de dados
+// Create connection
+$conexao = mysqli_connect($servername, $username, $password, $database);
+
+?>
---
+++ Dashboard2/index.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'conexao/conexao.php';
+
+$sql = "SELECT * FROM lucros";
+$buscar = mysqli_query($conexao,$sql);
+
+#chart.js - Preparando valores# 
+
+$mes = '';
+$ano_2018 = '';
+$ano_2019 = '';
+
+while ($dados = mysqli_fetch_array($buscar)) {
+
+    $mes = $mes . '"' . $dados['mes']. '",';
+    $ano_2018 = $ano_2018 . '"' . $dados['ano_2018']. '",';
+    $ano_2019 = $ano_2019 . '"' . $dados['ano_2019']. '",';
+
+    $mes = trim($mes, ','); #tira os espaços da variável
+    $ano_2018 = trim($ano_2018, ','); #tira os espaços da variável
+    $ano_2019 = trim($ano_2019, ','); #tira os espaços da variável
+    
+    
+}
+
+?>
