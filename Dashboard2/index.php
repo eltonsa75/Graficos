74cc8294db919ac996cfa65125ac583e68fb7f71
Elton <eltonsa75@hotmail.com>
2021-06-23 09:39:00 -0300

    Finalizando o gráfico linha

--- Dashboard2/index.php
+++ Dashboard2/index.php
@@ -29,12 +29,21 @@ while ($dados = mysqli_fetch_array($buscar)) {
 <!DOCYTPE html>
 <html>
 <head>
-    <title>Gráfico Linha</title>
+<title>Gráfico Linha</title>
+	<!-- CDN do Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ 
+	<!-- CDN do Chart.js -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 </head>
 <body>
 
+<div class="container" style="background-color: #f3f3f3">
+  <canvas id="Linha"></canvas>
+</div>
+
 <script type="text/javascript">
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('Linha').getContext('2d');
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
@@ -54,17 +63,15 @@ var myLineChart = new Chart(ctx, {
             borderColor: 'rgba(0,255,255)',
             borderWidth: 3
         }]
-  }
+  },
 
-  options: { scales :{yAxes : [{beginAtZero : false}], xAxes : [
-      {autoskip: true, maxTicketsLimit: 20}]}},
-      tooltips: {mode: 'index'}
-      Legend : {display: true,position: 'top', labels:{fontColor
-        : 'rgb(255,255,255)', font-size: 16}}
+  options: { 
+      scales: {scales :{yAxes : [{beginAtZero : false}], xAxes : [{autoskip: true, maxTicketsLimit: 20 }]}},
+      tooltips: {mode: 'index'},
+      legend:{display: true,position: 'top', labels:{fontColor: 'rgb(0,0,0)', fontSize: 16}}
+  }
 });
 </script>
-
-
 
 </body>
 </html>
